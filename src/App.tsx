9ac3fd946e00764817c91d4b09bee512f6e4e495
Martin Fogliacco <fogliaccomartin3@gmail.com>
2024-01-25 17:47:07 -0300

    Project base

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { 
+  Navbar,
+  Hero,
+  AboutUs,
+  PracticeAreas,
+  Testimonials,
+  MayaMaceyFoundation,
+  Contact,
+  Footer
+} from "./components";
+
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <AboutUs />
+      <PracticeAreas />
+      <Testimonials />
+      <MayaMaceyFoundation />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+export { default as AboutUs } from "./about-us/Index";
+export { default as Contact } from "./contact/Index";
+export { default as Footer } from "./footer/Index";
+export { default as Hero } from "./hero/Index";
+export { default as MayaMaceyFoundation } from "./maya-macey-foundation/Index";
+export { default as Navbar } from "./navbar/Index";
+export { default as PracticeAreas } from "./practice-areas/Index";
+export { default as Testimonials } from "./testimonials/Index";
