0ec12c79359c7fd5f6b6f68884f7d29b0f05f4c2
Martin Fogliacco <fogliaccomartin3@gmail.com>
2024-01-26 17:41:58 -0300

    feat: Hero component

--- src/components/hero/Index.tsx
+++ src/components/hero/Index.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { HeroBackground } from '../../utils/images'
 
 const Index = () => {
   return (
-    <div>Index</div>
+    <header className='h-screen w-full flex flex-col justify-center'>
+      <div className='z-[1] xs:pl-4 sm:pl-4 md:pl-0'>
+        <h1 className='font-raleway font-bold xs:text-6xl sm:text-6xl text-neutral'>Macey Law</h1>
+        <p className='font-lato font-medium xs:text-xl sm:text-xl text-neutral/70 pt-2'>Decades of Victory, A Tradition of Excellence</p>
+      </div>
+
+      <div className="absolute top-0 left-0 w-full h-full bg-cover bg-center" style={{ backgroundImage: `url(${HeroBackground})`, transform: 'rotateX(360deg)' }}>
+        <div className='h-full w-full bg-black/60'></div>
+      </div>
+    </header>
   )
 }
 
