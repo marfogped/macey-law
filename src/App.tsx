01c44e774ff14e56934b506eaf9270241b7e743f
Martin Fogliacco <fogliaccomartin3@gmail.com>
2024-01-26 17:42:24 -0300

    feat: styles at main tag

--- src/App.tsx
+++ src/App.tsx
@@ -15,13 +15,15 @@ function App() {
   return (
     <>
       <Navbar />
-      <Hero />
-      <AboutUs />
-      <PracticeAreas />
-      <Testimonials />
-      <MayaMaceyFoundation />
-      <Contact />
-      <Footer />
+      <main className="xl:w-[80%] mx-auto xs:w-full sm:w-full">
+        <Hero />
+        <AboutUs />
+        <PracticeAreas />
+        <Testimonials />
+        <MayaMaceyFoundation />
+        <Contact />
+        <Footer />
+      </main>
     </>
   )
 }
