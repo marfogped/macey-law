4193ea2eeca76435a989f7d1e39d72a365b7d60f
Martin Fogliacco <fogliaccomartin3@gmail.com>
2024-01-26 17:41:23 -0300

    feat: About component

--- src/components/about-us/Index.tsx
+++ src/components/about-us/Index.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
-
 const Index = () => {
   return (
-    <div>Index</div>
+    <section className='w-full h-max grid xs:grid-cols-1 sm:grid-cols-1 lg:grid-cols-5 px-4 py-16 mx-auto'>
+      <div className="lg:col-span-2">
+        <h2 className='section-title'>Leaders in Sophisticated and Group Litigation <span className='text-lightBlue'>Excellence</span></h2>
+      </div>
+
+      <div className='xs:mt-10 sm:mt-10 lg:col-span-3'>
+        <p className='section-text mb-6'>Macey Law is a premier international law firm, dedicated to defending individuals charged with or under investigation for the commission of state or federal crimes.</p>
+        <p className='section-text mb-6'>With over 20 years of legal experience, Top Criminal Defense Attorney David Macey has the knowledge and experience to effectively advocate for you. As a former prosecutor specializing in narcotics and organized crimes, he understands the serious consequences you are facing and will fight aggressively for you. Macey Law is one of the Top Criminal Law Firms in Miami.</p>
+      </div>
+    </section>
   )
 }
 
