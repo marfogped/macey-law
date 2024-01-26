335ec8555330e485a8fe4213042f2ebdfb091ed9
Martin Fogliacco <fogliaccomartin3@gmail.com>
2024-01-26 16:07:44 -0300

    refactor: images moved to img folder into assets folder

---
+++ src/utils/images.ts
@@ -0,0 +1,14 @@
+import HeroBackground from "../assets/img/background/hero-background.webp";
+
+import MaceyFullWhite from "../assets/img/logo/macey-law-full-white.webp"; 
+import MaceyLogoTransparent from "../assets/img/logo/macey-law-logo-transparent.webp"; 
+import MaceyLogoWhite from "../assets/img/logo/macey-law-logo-white.webp";
+import MayaMaceyFoundation from "../assets/img/logo/maya-macey-foundation.webp";
+
+export {
+    HeroBackground,
+    MaceyFullWhite,
+    MaceyLogoTransparent,
+    MaceyLogoWhite,
+    MayaMaceyFoundation
+}
