e76a15056a6625f404eb084deb7905d85a452dd5
Martin <martin@poreldeporte.com>
2024-01-27 21:29:38 -0300

    fix: cols & rows spaces types at card component, and responsive styles

--- src/components/practice-areas/Index.tsx
+++ src/components/practice-areas/Index.tsx
@@ -6,12 +6,12 @@ const Index: React.FC = () => {
   return (
     <section className='w-full h-max bg-[#0F0F1C] py-12'>
       <div className='section-container flex flex-col items-center gap-5'>
-        <header className='h-max w-full bg-neutral/10 py-4 flex flex-col items-center justify-center'>
+        <header className='h-max xs:w-[90%] sm:w-[90%] mx-auto bg-neutral/10 py-4 flex flex-col items-center justify-center'>
           <h2 className='section-title text-neutral text-center'>Leading the Way in Advanced and Group Litigation</h2>
           <p className='section-text text-neutral/80 pt-4'>View our Practice Areas</p>
         </header>
 
-        <div className="w-full h-max grid grid-cols-5 grid-flow-row-dense gap-5">
+        <div className="xs:w-[90%] sm:w-[90%] mx-auto md:w-full h-max grid xs:grid-cols-2 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 xxl:grid-cols-5 grid-flow-row-dense gap-5">
           {PRACTICE_AREAS.map((card) => (
             <Card key={card._id} card={card} />
           ))}
--- src/components/practice-areas/components/Card.tsx
+++ src/components/practice-areas/components/Card.tsx
@@ -3,20 +3,29 @@ import { PracticeAreaProps } from '../../../utils/constants';
 
 type CardProps = {
     card: PracticeAreaProps;
+};
+
+type ColSpanClasses = {
+    [key: string]: string;
+  };
+  
+  type RowSpanClasses = {
+    [key: string]: string;
+  };
+  
+  const colSpanClasses: ColSpanClasses = {
+    '1': 'col-span-1',
+    '2': 'col-span-2',
+    // Añadir más si es necesario
+  };
+  
+  const rowSpanClasses: RowSpanClasses = {
+    '1': 'row-span-1',
+    '2': 'row-span-2',
+    // Añadir más si es necesario
   };
 
 const Card: React.FC<CardProps> = ({ card }) => {
-
-    const colSpanClasses = {
-        1: 'col-span-1',
-        2: 'col-span-2',
-      };
-    
-      const rowSpanClasses = {
-        1: 'row-span-1',
-        2: 'row-span-2',
-      };
-
     return (
         <article
         className={`bg-cover bg-center cursor-pointer ${colSpanClasses[card.colSpan]} ${rowSpanClasses[card.rowSpan]}`}
