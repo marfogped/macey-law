40c2dffccfa3490bdde403b37b99330599ac1cfb
Martin <martin@poreldeporte.com>
2024-01-29 11:01:23 -0300

    fix: footer items aligment

--- src/components/footer/Index.tsx
+++ src/components/footer/Index.tsx
@@ -6,15 +6,15 @@ import { ScrollTo } from '..';
 const Index: React.FC = () => {
   return (
     <footer className='h-max w-full'>
-      <div className='w-full h-full flex items-center justify-center mt-6 mb-16'>
-        <img src={MaceyFullWhite} alt="macey-full-logo" className='h-64' />
+      <div className='xs:w-[90%] sm:w-[90%] lg:w-full mx-auto h-full flex items-center justify-center xs:my-16 sm:my-16 md:mt-6'>
+        <img src={MaceyFullWhite} alt="macey-full-logo" className='h-64 w-auto' />
       </div>
 
-      <div className='bg-[#1C3C7B] py-4'>
-        <div className='h-max flex xs:flex-col sm:flex-col lg:flex-row items-center justify-between section-container gap-4'>
+      <div className='bg-[#1C3C7B] py-4 xs:px-4 sm:px-4 md:px-0'>
+        <div className='h-max flex xs:flex-col sm:flex-col lg:flex-row xs:items-start sm:items-start md:items-center justify-between section-container xs:gap-8 sm:gap-8 md:gap-4 '>
           <div className='text-neutral'>© 2024 All Rights Reserved.</div>
 
-          <ul className='flex gap-4'>
+          <ul className='flex xs:flex-col sm:flex-col md:flex-row gap-4'>
           {
               NAV_ITEMS.map((label) => (
                 <li 
@@ -27,7 +27,7 @@ const Index: React.FC = () => {
             }
           </ul>
 
-          <ul className='flex gap-4'>
+          <ul className='flex xs:flex-col sm:flex-col md:flex-row gap-4'>
             {
               FOOTER_LINKS.map((label) => (
                 <li 
