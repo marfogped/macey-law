f593f9419c7d0a6b2746ee4384972fcfbc1bdb8a
BelenDeAmorrortu <belendeamorrortu@gmail.com>
2024-03-01 15:34:42 -0300

    add: Practice Areas images and imports at images.ts & constants.ts

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -4,9 +4,90 @@ import MaceyFullWhite from "../assets/img/logo/macey-law-full-white.webp";
 import MaceyLogoTransparent from "../assets/img/logo/macey-law-logo-transparent.webp";
 import MayaMaceyFoundation from "../assets/img/logo/maya-macey-foundation.webp";
 
+import ComputerFraud from "../assets/img/areas-backgrounds/computer-fraud.jpeg";
+import CyberStalking from "../assets/img/areas-backgrounds/cyberstalking.jpeg";
+import DuiDwi from "../assets/img/areas-backgrounds/dui-dwi.webp";
+import Emblezzment from "../assets/img/areas-backgrounds/emblezzment.webp";
+import ExpungingRecords from "../assets/img/areas-backgrounds/expunging-records.webp";
+import FederalCrimesTrials from "../assets/img/areas-backgrounds/federal-criminal-trials.webp";
+import HealthcareFraud from "../assets/img/areas-backgrounds/healthcare-fraud.webp";
+import InsiderTrading from "../assets/img/areas-backgrounds/insider-trading.webp";
+import InsuranceFraud from "../assets/img/areas-backgrounds/insurance-fraud.webp";
+import InternetFraud from "../assets/img/areas-backgrounds/internet-fraud.jpeg";
+import IrsViolations from "../assets/img/areas-backgrounds/irs-violations.webp";
+import MortgageFraud from "../assets/img/areas-backgrounds/mortgage-fraud.webp";
+import TelemarketingFraud from "../assets/img/areas-backgrounds/telemarketing-fraud.webp";
+import TradeSecretTheft from "../assets/img/areas-backgrounds/trade-secret-theft.webp";
+import WhiteCollarCrimes from "../assets/img/areas-backgrounds/white-collar-crimes.webp";
+import AntitrustViolations from "../assets/img/areas-backgrounds/antitrust-violations.webp";
+import Conspiraces from "../assets/img/areas-backgrounds/conspiraces.webp";
+import DrugTrafficking from "../assets/img/areas-backgrounds/drug-trafficking.webp";
+import EnviromentalLawViolations from "../assets/img/areas-backgrounds/environmental-law-violations.webp";
+import FederalCriminalAppeals from "../assets/img/areas-backgrounds/federal-criminal-appeals.webp";
+import Kickbacks from "../assets/img/areas-backgrounds/kickbacks.webp";
+import RicoCases from "../assets/img/areas-backgrounds/rico-cases.webp";
+import SexCrimes from "../assets/img/areas-backgrounds/sex-crimes.webp";
+import Bribery from "../assets/img/areas-backgrounds/bribery.webp";
+import BusinessFraud from "../assets/img/areas-backgrounds/business-fraud.webp";
+import AssetForfeiture from "../assets/img/areas-backgrounds/asset-forfeiture.avif";
+import BankFraud from "../assets/img/areas-backgrounds/bank-fraud.avif";
+import BankruptcyFraud from "../assets/img/areas-backgrounds/bankruptcy-fraud.avif";
+import Counterfeiting from "../assets/img/areas-backgrounds/counterfeiting.webp";
+import CreditCardFraud from "../assets/img/areas-backgrounds/credit-card-fraud.avif";
+import DrugCases from "../assets/img/areas-backgrounds/drug-cases.webp";
+import GovernmentFraud from "../assets/img/areas-backgrounds/government-fraud.avif";
+import InternationalCrimes from "../assets/img/areas-backgrounds/international-crimes.webp";
+import InternationalExtradition from "../assets/img/areas-backgrounds/international-extradition.avif";
+import MoneyLaundering from "../assets/img/areas-backgrounds/money-laundering.avif";
+import SecurityViolation from "../assets/img/areas-backgrounds/security-violations.webp";
+import StateFelonyAppeals from "../assets/img/areas-backgrounds/state-felony-appeals.webp";
+import StateFelonyTrials from "../assets/img/areas-backgrounds/state-felony-trials.webp";
+import FinancialFraud from "../assets/img/areas-backgrounds/financial-frauds.webp";
+import MailFraud from "../assets/img/areas-backgrounds/mail-fraud.jpeg";
+
 export {
   HeroBackground,
   MaceyFullWhite,
   MaceyLogoTransparent,
   MayaMaceyFoundation,
+  ComputerFraud,
+  CyberStalking,
+  DuiDwi,
+  Emblezzment,
+  ExpungingRecords,
+  FederalCrimesTrials,
+  HealthcareFraud,
+  InsiderTrading,
+  InsuranceFraud,
+  InternetFraud,
+  IrsViolations,
+  MortgageFraud,
+  TelemarketingFraud,
+  TradeSecretTheft,
+  WhiteCollarCrimes,
+  AntitrustViolations,
+  Conspiraces,
+  DrugTrafficking,
+  EnviromentalLawViolations,
+  FederalCriminalAppeals,
+  Kickbacks,
+  RicoCases,
+  SexCrimes,
+  Bribery,
+  BusinessFraud,
+  AssetForfeiture,
+  BankFraud,
+  BankruptcyFraud,
+  Counterfeiting,
+  CreditCardFraud,
+  DrugCases,
+  GovernmentFraud,
+  InternationalCrimes,
+  InternationalExtradition,
+  MoneyLaundering,
+  SecurityViolation,
+  StateFelonyAppeals,
+  StateFelonyTrials,
+  FinancialFraud,
+  MailFraud,
 };
