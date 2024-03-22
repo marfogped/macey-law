c5072f3768f48177f0dd3eca750e60a9ae6ea040
Martin Fogliacco <fogliaccomartin3@gmail.com>
2024-03-22 15:13:42 -0300

    fix: Testimonials names

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -439,19 +439,19 @@ export const NAV_ITEMS = [
 export const TESTIMONIALS = [
     {
         id: "1",
-        author: "Jane Doe",
+        author: "María Gonzalez",
         description:
             "Faced with a daunting legal challenge, I sought the expertise of Macey Law. Their team provided not only legal representation but also constant support and reassurance throughout the process. Their strategic approach played a crucial role in dealing with the complexities of my case, leading to an outcome better than I had hoped.",
     },
     {
         id: "2",
-        author: "John Doe",
+        author: "Daniel Smith",
         description:
             "The team at Macey Law was pivotal in my legal challenge, demonstrating courtroom expertise from the start. They handled my case with professionalism and empathy, addressing every concern and simplifying the complex legal process. The positive resolution they achieved has greatly impacted my life, and I wholeheartedly recommend them for their dedication and exceptional legal representation.",
     },
     {
         id: "3",
-        author: "Jane Doe",
+        author: "Alex Zhang",
         description:
             "In my legal ordeal, Macey Law was a beacon of hope. They provided more than legal expertise; their reassurance and strategic approach were instrumental in navigating my case's complexities. I am immensely grateful for their outstanding service and support, which led to a better outcome than I expected. Their ability to simplify a challenging situation is why I highly recommend them.",
     },
